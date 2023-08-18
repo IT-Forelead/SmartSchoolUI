79527836b630a93b6f99bfca473918cc511fa793
Prince <prince777_98@mail.ru>
2023-08-18 10:21:55 +0500

    sort by group name

--- src/app/dashboard/timetable/page.tsx
+++ src/app/dashboard/timetable/page.tsx
@@ -1,30 +1,23 @@
-"use client"
-import Loader from '@/components/client/Loader';
-import { Button } from '@/components/ui/button';
-import { useTimeTable } from '@/hooks/useTimeTable';
-import { SolarDownloadSquareBroken } from '@/icons/DownloadIcon';
+"use client";
+import Loader from "@/components/client/Loader";
+import { Button } from "@/components/ui/button";
+import { useTimeTable } from "@/hooks/useTimeTable";
+import { SolarDownloadSquareBroken } from "@/icons/DownloadIcon";
 
 export default function TimeTablePage() {
   const timeTableResponse = useTimeTable();
-  const timetable = timeTableResponse?.data?.data ?? {}
-  const groups: string[] = Object.keys(timetable) ?? []
+  const timetable = timeTableResponse?.data?.data ?? {};
+  const groups: string[] = Object.keys(timetable) ?? [];
   const weekdays = [
     "Dushanba",
     "Seshanba",
     "Chorshanba",
     "Payshanba",
     "Juma",
-    "Shanba"
+    "Shanba",
   ];
 
-  const weekdaysThreeLetter = [
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat"
-  ]
+  const weekdaysThreeLetter = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   function translateWeekday(uwd: string) {
     const weekdayMap = new Map();
@@ -32,49 +25,81 @@ export default function TimeTablePage() {
     for (let i = 0; i < weekdays.length; i++) {
       weekdayMap.set(weekdays[i], weekdaysThreeLetter[i]);
     }
-    return weekdayMap.get(uwd)
+    return weekdayMap.get(uwd);
   }
 
   return (
-    <div className='p-2 px-5'>
-      {!false ?
-        <table className='w-full text-sm border'>
+    <div className="p-2 px-5">
+      {!false ? (
+        <table className="w-full text-sm border">
           <thead>
-            <tr className='text-gray-500 border'>
-              <th className="p-2 text-white bg-gray-400 border">Dars jadvali</th>
-              {weekdays.map(day => {
+            <tr className="text-gray-500 border bg-gray-100">
+              <th className="p-2 text-white bg-gray-500 border">
+                Dars jadvali
+              </th>
+              {weekdays.map((day) => {
                 return (
-                  <th key={day} className="p-2 border">{day}</th>
-                )
+                  <th key={day} className="p-2 border">
+                    {day}
+                  </th>
+                );
               })}
             </tr>
           </thead>
           <tbody>
-            {
-              groups?.sort()?.map((item) => {
+            {groups
+              ?.sort((a, b) => {
+                const [aNumber, aLetter] = a.split("-");
+                const [bNumber, bLetter] = b.split("-");
+
+                if (aNumber !== bNumber) {
+                  return parseInt(aNumber) - parseInt(bNumber);
+                } else {
+                  if (aLetter < bLetter) return -1;
+                  if (aLetter > bLetter) return 1;
+                  return 0;
+                }
+              })
+              ?.map((item, idx) => {
                 return (
-                  <tr key={item} className='border'>
-                    <td className="p-1 font-medium text-center text-gray-500 border">{item}</td>
-                    {weekdays.map(day => {
+                  <tr
+                    key={item}
+                    className={`border ${
+                      idx % 2 !== 1 ? "bg-white" : "bg-gray-100"
+                    }`}
+                  >
+                    <td className="p-1 font-medium text-center text-gray-500 border">
+                      {item}
+                    </td>
+                    {weekdays.map((day) => {
                       return (
-                        <td key={day} className="p-1 font-medium text-gray-500 border">
-                          <ol className='divide-y'>
-                            {timetable[item][translateWeekday(day)]?.map((subject: any) => {
-                              return <li className='p-2' key={subject}>{subject?.moment}. {subject?.subjectName}</li>
-                            })
-                            }
+                        <td
+                          key={day}
+                          className="p-1 align-top font-medium text-gray-500 border"
+                        >
+                          <ol className="space-y-1">
+                            {timetable[item][translateWeekday(day)]?.map(
+                              (subject: any) => {
+                                return (
+                                  <li className="p-1 border" key={subject}>
+                                    {subject?.moment}. {subject?.subjectName}
+                                    <p className="text-[11px] text-right capitalize font-bold">{subject?.teacherName}</p>
+                                  </li>
+                                );
+                              }
+                            )}
                           </ol>
                         </td>
-                      )
+                      );
                     })}
                   </tr>
-                )
-              })
-            }
+                );
+              })}
           </tbody>
-        </table> :
+        </table>
+      ) : (
         <Loader />
-      }
+      )}
     </div>
-  )
+  );
 }

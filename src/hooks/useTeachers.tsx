a5503c4403c01ceb7741f84c330f59e6fb01397b
surojiddin <isurojiddin@gmail.com>
2023-12-04 17:13:47 +0500

    api changed

--- src/hooks/useTeachers.tsx
+++ src/hooks/useTeachers.tsx
@@ -44,7 +44,7 @@ export const addSubjectToTeacherFunc = async (data: AddSubjects) => {
 };
 
 export const addQrCodeToTeacher = async (data: AddQrCode) => {
-  return await axios.get<any>(`/student/add-qrcode/${data.personId}`);
+  return await axios.post<any>('/student/add-qrcode', data);
 };
 
 export const approveTeacherDocAsAdmin = async (data: ApproveAsAdmin) => {

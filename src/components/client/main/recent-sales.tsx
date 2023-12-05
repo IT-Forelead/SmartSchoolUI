201504e5c9c2ae2479f61a25990589cf8a8e45aa
surojiddin <isurojiddin@gmail.com>
2023-12-05 19:41:33 +0500

    dashboard improved

--- src/components/client/main/recent-sales.tsx
+++ src/components/client/main/recent-sales.tsx
@@ -5,12 +5,12 @@ export function RecentSales() {
     <div className="space-y-8">
       <div className="flex items-center">
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Olivia Martin</p>
+          <p className="text-sm font-medium leading-none">Barcha o'qituvchilar</p>
           <p className="text-sm text-muted-foreground">
             olivia.martin@email.com
           </p>
         </div>
-        <div className="ml-auto font-medium">+$1,999.00</div>
+        <div className="ml-auto text-xl font-medium">37</div>
       </div>
       <div className="flex items-center">
         <div className="ml-4 space-y-1">

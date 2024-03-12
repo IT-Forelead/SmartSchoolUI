d3b15bd921e652718934a464095b602cd55ec440
newuzbdev <125820900+newuzbdev@users.noreply.github.com>
2024-03-12 10:49:43 +0500

    merge branch message

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -258,7 +258,100 @@ export default function MessagesPage() {
           <Button onClick={handleSubmit}>Qidirish</Button>
         </div>
       </div>
-      <div className="rounded-md border dark:border-slate-600">
+      <div className="flex justify-between">
+        <div className="flex items-center justify-start space-x-2 py-4">
+          <div className="flex-1">
+            <div className="text-muted-foreground flex items-start space-x-2 text-sm dark:text-slate-400 ">
+              <div className="rounded-full pt-2 text-gray-800 dark:text-gray-200">Jami</div>
+              <div className="pt-2 text-gray-800 dark:text-gray-200">:</div>
+              <div className="mr-2 mt-2 font-bold text-gray-800 dark:text-gray-200">
+                {stats?.total}
+              </div>
+              <Separator orientation="vertical" />
+              <div className="upparcase rounded-full bg-green-600 p-2 text-center text-white">
+                {translateSMSStatus("Delivered")}
+              </div>
+              <div className="pt-2 text-gray-800 dark:text-gray-200">:</div>
+              <div className="mt-2 font-bold text-gray-800 dark:text-gray-200">
+                {stats?.delivered}
+              </div>
+              <Separator orientation="vertical" />
+              <div className="rounded-full  bg-blue-600 p-2 text-center text-white">
+                {translateSMSStatus("Sent")}
+              </div>
+              <div className="pt-2 text-gray-800 dark:text-gray-200">:</div>
+              <div className="mt-2 font-bold text-gray-800 dark:text-gray-200">{stats?.sent}</div>
+              <Separator orientation="vertical" />
+              <div className="rounded-full bg-yellow-600 p-2 text-center text-white">
+                {translateSMSStatus("NotDelivered")}
+              </div>
+              <div className="pt-2 text-gray-800 dark:text-gray-200">:</div>
+              <div className="mt-2 font-bold text-gray-800 dark:text-gray-200">
+                {stats?.notDelivered}
+              </div>
+              <Separator orientation="vertical" />
+
+              <div className="rounded-full bg-red-600 p-2 text-center text-white ">
+                {translateSMSStatus("Failed")}
+              </div>
+              <div className="pt-2 text-gray-800 dark:text-gray-200">:</div>
+              <div className="mt-2 font-bold text-gray-800 dark:text-gray-200">
+                {stats?.failed}
+              </div>
+              <Separator orientation="vertical" />
+
+              <div className="rounded-full bg-gray-600 p-2 text-center text-white">
+                {translateSMSStatus("Undefined")}{" "}
+              </div>
+              <div className="pt-2 text-gray-800 dark:text-gray-200">:</div>
+              <div className="pt-2 font-bold text-gray-800 dark:text-gray-200">
+                {(stats?.undefined ?? 0) + (stats?.transmitted ?? 0)}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-end pb-0">
+            <div className="space-x-2">
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious
+                      href="#"
+                      onClick={() => setCurrentPage(currentPage - 1)}
+                    />
+                  </PaginationItem>
+                  {paginate(currentPage, pagesCount).map((page) => {
+                    return (
+                      <PaginationItem key={page}>
+                        {page == 0 ? (
+                          <PaginationEllipsis />
+                        ) : (
+                          <PaginationLink
+                            href="#"
+                            isActive={page == currentPage}
+                            onClick={() => {
+                              setCurrentPage(page);
+                            }}
+                          >
+                            {page}
+                          </PaginationLink>
+                        )}
+                      </PaginationItem>
+                    );
+                  })}
+                  <PaginationItem>
+                    <PaginationNext
+                      href="#"
+                      onClick={() => setCurrentPage(currentPage + 1)}
+                    />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 rounded-md border dark:border-slate-600">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -308,71 +401,44 @@ export default function MessagesPage() {
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1">
-          <div className="text-muted-foreground flex items-start space-x-4 text-sm dark:text-slate-400">
-            <div>Jami: {stats?.total}</div>
-            <Separator orientation="vertical" />
-            <div>
-              {translateSMSStatus("Delivered")}: {stats?.delivered}
-            </div>
-            <Separator orientation="vertical" />
-            <div>
-              {translateSMSStatus("Sent")}: {stats?.sent}
-            </div>
-            <Separator orientation="vertical" />
-            <div>
-              {translateSMSStatus("NotDelivered")}: {stats?.notDelivered}
-            </div>
-            <Separator orientation="vertical" />
-            <div>
-              {translateSMSStatus("Failed")}: {stats?.failed}
-            </div>
-            <Separator orientation="vertical" />
-            <div>
-              {translateSMSStatus("Undefined")}:{" "}
-              {(stats?.undefined ?? 0) + (stats?.transmitted ?? 0)}
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="space-x-2">
-            <Pagination>
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious
-                    href="#"
-                    onClick={() => setCurrentPage(currentPage - 1)}
-                  />
-                </PaginationItem>
-                {paginate(currentPage, pagesCount).map((page) => {
-                  return (
-                    <PaginationItem key={page}>
-                      {page == 0 ? (
-                        <PaginationEllipsis />
-                      ) : (
-                        <PaginationLink
-                          href="#"
-                          isActive={page == currentPage}
-                          onClick={() => {
-                            setCurrentPage(page);
-                          }}
-                        >
-                          {page}
-                        </PaginationLink>
-                      )}
-                    </PaginationItem>
-                  );
-                })}
-                <PaginationItem>
-                  <PaginationNext
-                    href="#"
-                    onClick={() => setCurrentPage(currentPage + 1)}
-                  />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
-          </div>
+
+      <div className="flex items-center justify-end pt-10">
+        <div className="space-x-2">
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  href="#"
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                />
+              </PaginationItem>
+              {paginate(currentPage, pagesCount).map((page) => {
+                return (
+                  <PaginationItem key={page}>
+                    {page == 0 ? (
+                      <PaginationEllipsis />
+                    ) : (
+                      <PaginationLink
+                        href="#"
+                        isActive={page == currentPage}
+                        onClick={() => {
+                          setCurrentPage(page);
+                        }}
+                      >
+                        {page}
+                      </PaginationLink>
+                    )}
+                  </PaginationItem>
+                );
+              })}
+              <PaginationItem>
+                <PaginationNext
+                  href="#"
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </div>
